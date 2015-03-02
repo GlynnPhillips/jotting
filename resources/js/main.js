b33e181cadc77518bc8ef0f915365907b188e03b
Glynn Phillips <g.phillips@nature.com>
2015-03-02 13:47:44 +0000

    adding basic admin styles

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,11 +1,22 @@
 if ("geolocation" in navigator) {
-	navigator.geolocation.getCurrentPosition(function(position) {
-		var latField = document.getElementById('lat');
-		var longField = document.getElementById('long');
+	var posButton =  document.getElementById('find-pos');
 
-		latField.value = position.coords.latitude;
-		longField.value =  position.coords.longitude;
-	});
+	posButton.addEventListener("click", function() {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var latField = document.getElementById('lat');
+			var longField = document.getElementById('long');
+			var latDisplay = document.getElementById('fake-lat');
+			var longDisplay = document.getElementById('fake-long');
+
+			latField.value = position.coords.latitude;
+			longField.value = position.coords.longitude;
+
+			latDisplay.innerHTML = position.coords.latitude;
+			longDisplay.innerHTML = position.coords.longitude;
+		});
+	}, false);
+
+	
 } else {
 
 }
